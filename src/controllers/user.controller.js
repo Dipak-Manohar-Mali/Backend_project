511cf05b7322d0ee214c3d2df994fa72884524be
Dipak-Manohar-Mali <dipakmali20003@gmail.com>
2024-03-14 15:19:32 +0530

    commit

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,22 +3,26 @@ import { ApiError } from "../utils/ApiError.js";
 import { User }  from "../models/user.model.js"
 import {uploadOnCloudinary} from "../utils/cloudinary.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
+import jwtfrom from "jsonwebtoken";
 
-const generateAccessAndRefreshTokens = async(userId)=>
+
+const generateAccessAndRefereshTokens = async(userId)=>
 {
     try {
         const user = await User.findById(userId)
-        const accessToken = user.generateAccessToken()
-        const refreshToken = user.generateRfreshToken()
-        user.refreshToken = refreshToken;
-        await user.save({
-            validateBeforeSave: false
-        })
+        const accessToken =  user.generateAccessToken()
+        const refreshToken =  user.generateRefreshToken()
 
-        return { accessToken ,refreshToken};
-        
+        console.log(accessToken);
+        console.log(refreshToken);
+        user.refreshToken = refreshToken
+        await user.save({ validateBeforeSave: false })
+
+        return {accessToken, refreshToken}
+
+
     } catch (error) {
-        throw new ApiError(500,"Something went wrong while generating refresh and access token")
+        throw new ApiError(500, "Something went wrong while generating referesh and access token")
     }
 }
 
@@ -123,7 +127,7 @@ const loginUser = asyncHandler (async (req,res) => {
 
     const {email,username,password} = req.body
 
-    if(!username || !email)
+    if(!(username || email))
     {
         throw new ApiError(400,"username or password is required")
     }
@@ -136,13 +140,13 @@ const loginUser = asyncHandler (async (req,res) => {
         throw new ApiError(404,"User Does Not Exist")
     }
 
-    const isPasswordValid = await user.isPassWordCorrect(password)
+    const isPasswordValid = await user.isPasswordCorrect(password)
     if(!isPasswordValid)
     {
         throw new ApiError(401,"Invalid user Credential")
     }
 
-    const {accessToken,refreshToken} = await generateAccessAndRefreshTokens(user._id)
+    const {accessToken, refreshToken} = await generateAccessAndRefereshTokens(user._id)
 
     const loggedInUser = await User.findById(user._id).
     select("-password -refreshToken");
@@ -191,10 +195,60 @@ const logoutUser = asyncHandler(async(req,res)=>{
     json(new ApiResponse(200,{},"User logged Out"))
 })
 
+const refreshAccessToken = asyncHandler(async(req,res)=>{
+   const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+   
+   if(!incomingRefreshToken)
+   {
+    throw new ApiError(401,"unauthorized reques")
+   }
+
+   try {
+    const decodedToken = jwt.verify(
+     incomingRefreshToken,
+     process.env.REFRESH_TOKEN_SECRET
+    )
+ 
+    const user = await User.findById(decodedToken?._id)
+ 
+    if(!user)
+    {
+     throw new ApiError(401,"Invalid RefreshToken")
+    }
+ 
+    if(incomingRefreshToken !== user?.refreshToken)
+    {
+     throw new ApiError(401,"Refresh token is expired or used")
+    }
+ 
+    const options = {
+     httpOnly:true,
+     secure:true
+    }
+ 
+   const {accessToken,newRefreshToken}= await generateAccessAndRefereshTokens(user._id)
+    return res.status(200).
+             cookie("accessToken",accessToken,options).
+             cookie("refreshToken",newRefreshToken,options).
+             json(
+                 new ApiResponse(
+                     200,
+                     {
+                         accessToken,refreshToken:newRefreshToken
+                     },
+                     "Access token refreshed"
+ 
+                 )
+             )
+   } catch (error) {
+    throw new ApiError(401,error?.message || "Invalid Refresh token")
+   }
+})
 
 
 export {
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccessToken
 }
--- src/models/user.model.js
+++ src/models/user.model.js
@@ -53,45 +53,42 @@ const userSchema =new mongoose.Schema(
     
     )
 
-userSchema.pre("save",async function (next){
-    if(!this.isModified("password")) return next();
-
-    this.password= await bcrypt.hash(this.password,10)
-    next()
-
-})
-
-userSchema.methods.isPassWordCorrect = async function(password)
-{
-    return await bcrypt.compare(password,this.password)
+    userSchema.pre("save", async function (next) {
+        if(!this.isModified("password")) return next();
+    
+        this.password = await bcrypt.hash(this.password, 10)
+        next()
+    })
+    
+    userSchema.methods.isPasswordCorrect = async function(password){
+        return await bcrypt.compare(password, this.password)
+    }
+    
+    userSchema.methods.generateAccessToken = function(){
+        return jwt.sign(
+            {
+                _id: this._id,
+                email: this.email,
+                username: this.username,
+                fullName: this.fullName
+            },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+            }
+        )
+    }
+    userSchema.methods.generateRefreshToken = function(){
+        return jwt.sign(
+            {
+                _id: this._id,
+                
+            },
+            process.env.REFRESH_TOKEN_SECRET,
+            {
+                expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+            }
+        )
 }
 
-userSchema.methods.generateAccessToken = function(){
-    jwt.sign(
-       { 
-            _id:this._id,
-            email:this.email,
-            username:this.username,
-            fullName:this.fullName,
-        },
-        process.env.ACCESS_TOKEN_SECRET,
-        {
-            expiresIn:ProcessingInstruction.env.ACCESS_TOKEN_EXPIRY
-        }
-    )
-}
-
-userSchema.methods.generateRfreshToken = function(){
-    jwt.sign(
-        { 
-             _id:this._id,
-             
-         },
-         process.env.REFRESH_TOKEN_SECRET,
-         {
-             expiresIn:ProcessingInstruction.env.REFRESH_TOKEN_EXPIRY
-         }
-     )
-}
-
-export const User= mongoose.model("User",userSchema);
+export const User = mongoose.model("User", userSchema)
